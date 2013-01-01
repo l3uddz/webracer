63c395c94fe032d0765bbdb6322d5b0e1e74399d
Oleg Pudeyev <oleg@bsdpower.com>
2012-12-31 22:17:57 -0500

    Add xpath_first utility method

---
+++ owebunit/tests/xpath_first_test.py
@@ -0,0 +1,24 @@
+import unittest
+import lxml.etree
+import owebunit
+
+class AbsolutizeUrlTestCase(owebunit.WebTestCase):
+    def test_xpath_first(self):
+        xml = '<root><el><child><grandchild>text</grandchild></child></el></root>'
+        doc = lxml.etree.XML(xml)
+        
+        # should not raise
+        child = self.xpath_first(doc, '//child')
+        self.assertIsNot(child, None)
+        self.assertEqual('child', child.tag)
+        
+        try:
+            self.xpath_first(doc, '//nonexistent')
+            assert False
+        except AssertionError:
+            # ok
+            pass
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
