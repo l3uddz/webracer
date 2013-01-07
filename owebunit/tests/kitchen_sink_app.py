e6abe55d178b5c0bc22ab8d984636dad4d9f02c5
Oleg Pudeyev <oleg@bsdpower.com>
2013-01-06 21:18:31 -0500

    Extract kitchen sink app from the test for reuse

---
+++ owebunit/tests/kitchen_sink_app.py
@@ -0,0 +1,53 @@
+import bottle
+
+app = bottle.Bottle()
+
+@app.route('/ok')
+def ok():
+    return 'ok'
+
+@app.route('/internal_server_error')
+def internal_error():
+    bottle.abort(500, 'internal server error')
+
+@app.route('/redirect')
+def redirect():
+    bottle.redirect('/found', 302)
+
+@app.route('/redirect_to')
+def redirect():
+    bottle.redirect(bottle.request.query.target, 302)
+
+@app.route('/set_cookie')
+def set_cookie():
+    bottle.response.set_cookie('visited', 'yes')
+
+@app.route('/set_multiple_cookies')
+def set_multiple_cookies():
+    bottle.response.set_cookie('foo_a', 'a_value', expires=1)
+    bottle.response.set_cookie('foo_b', 'b_value', httponly=True)
+    bottle.response.set_cookie('foo_c', 'c_value', secure=True)
+
+@app.route('/read_cookie')
+def read_cookie():
+    return bottle.request.get_cookie('visited')
+
+@app.route('/get_param')
+def get_param():
+    return bottle.request.query.p
+
+@app.route('/param', method='POST')
+def param():
+    return bottle.request.forms.p
+
+@app.route('/get_content_length', method='POST')
+def get_content_length():
+    return bottle.request.headers.get('content-length')
+
+@app.route('/json/empty')
+def get_json_empty():
+    return '{}'
+
+@app.route('/json/hash')
+def get_json_empty():
+    return '{"a": "b"}'
--- owebunit/tests/kitchen_sink_test.py
+++ owebunit/tests/kitchen_sink_test.py
@@ -1,63 +1,11 @@
 import threading
 import time
 import owebunit
-import bottle
 import mock
 from owebunit.tests import utils
+from owebunit.tests import kitchen_sink_app
 
-app = bottle.Bottle()
-
-@app.route('/ok')
-def ok():
-    return 'ok'
-
-@app.route('/internal_server_error')
-def internal_error():
-    bottle.abort(500, 'internal server error')
-
-@app.route('/redirect')
-def redirect():
-    bottle.redirect('/found', 302)
-
-@app.route('/redirect_to')
-def redirect():
-    bottle.redirect(bottle.request.query.target, 302)
-
-@app.route('/set_cookie')
-def set_cookie():
-    bottle.response.set_cookie('visited', 'yes')
-
-@app.route('/set_multiple_cookies')
-def set_multiple_cookies():
-    bottle.response.set_cookie('foo_a', 'a_value', expires=1)
-    bottle.response.set_cookie('foo_b', 'b_value', httponly=True)
-    bottle.response.set_cookie('foo_c', 'c_value', secure=True)
-
-@app.route('/read_cookie')
-def read_cookie():
-    return bottle.request.get_cookie('visited')
-
-@app.route('/get_param')
-def get_param():
-    return bottle.request.query.p
-
-@app.route('/param', method='POST')
-def param():
-    return bottle.request.forms.p
-
-@app.route('/get_content_length', method='POST')
-def get_content_length():
-    return bottle.request.headers.get('content-length')
-
-@app.route('/json/empty')
-def get_json_empty():
-    return '{}'
-
-@app.route('/json/hash')
-def get_json_empty():
-    return '{"a": "b"}'
-
-utils.start_bottle_server(app, 8041)
+utils.start_bottle_server(kitchen_sink_app.app, 8041)
 
 class KitchenSinkTestCase(owebunit.WebTestCase):
     def test_simple(self):
