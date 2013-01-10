5a02f59749c6ec8cfb0a1a454c348775daf5a5c9
Oleg Pudeyev <oleg@bsdpower.com>
2013-01-10 06:01:01 -0500

    Add a setup.py for pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='owebunit',
+    version='0.1',
+    description='Comprehensive web application testing library',
+    author='Oleg Pudeyev',
+    author_email='oleg@bsdpower.com',
+    url='http://github.com/p/owebunit',
+    packages=['owebunit'],
+)
