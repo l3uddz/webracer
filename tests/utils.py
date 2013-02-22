51983546e3fe50ceaeb788a2eef5ad8def919b1d
Oleg Pudeyev <oleg@bsdpower.com>
2013-02-22 15:54:58 -0500

    Extract app_runner_setup_multiple for reuse

--- tests/utils.py
+++ tests/utils.py
@@ -1,54 +1,8 @@
-import bottle
-import threading
-import socket
-import time as _time
+import webracer.utils.runwsgi
 import sys
 import nose.tools
 
 py3 = sys.version_info[0] == 3
-
-class Server(bottle.WSGIRefServer):
-    def run(self, handler): # pragma: no cover
-        from wsgiref.simple_server import make_server, WSGIRequestHandler
-        if self.quiet:
-            base = self.options.get('handler_class', WSGIRequestHandler)
-            class QuietHandler(base):
-                def log_request(*args, **kw): pass
-            self.options['handler_class'] = QuietHandler
-        self.srv = make_server(self.host, self.port, handler, **self.options)
-        self.srv.serve_forever(poll_interval=0.1)
-
-def start_bottle_server(app, port, **kwargs):
-    server_thread = ServerThread(app, port, kwargs)
-    server_thread.daemon = True
-    server_thread.start()
-    
-    ok = False
-    for i in range(10):
-        try:
-            conn = socket.create_connection(('127.0.0.1', port), 0.1)
-        except socket.error as e:
-            _time.sleep(0.1)
-        else:
-            conn.close()
-            ok = True
-            break
-    if not ok:
-        import warnings
-        warnings.warn('Server did not start after 1 second')
-    
-    return server_thread.server
-
-class ServerThread(threading.Thread):
-    def __init__(self, app, port, server_kwargs):
-        threading.Thread.__init__(self)
-        self.app = app
-        self.port = port
-        self.server_kwargs = server_kwargs
-        self.server = Server(host='localhost', port=self.port, **self.server_kwargs)
-    
-    def run(self):
-        bottle.run(self.app, server=self.server, quiet=True)
 
 def app_runner_setup(module_name, app, port):
     app_runner_setup_multiple(module_name, [[app, port]])
@@ -58,19 +12,7 @@ def app_runner_setup_multiple(module_name, specs):
     # requiring all tests to import sys
     module = sys.modules[module_name]
     
-    def setup(self):
-        self.servers = []
-        for spec in specs:
-            if len(spec) == 2:
-                app, port = spec
-                kwargs = {}
-            else:
-                app, port, kwargs = spec
-            self.servers.append(start_bottle_server(app, port, **kwargs))
-    
-    def teardown(self):
-        for server in self.servers:
-            server.srv.shutdown()
+    setup, teardown = webracer.utils.runwsgi.app_runner_setup_multiple(*specs)
     
     assert not hasattr(module, 'setup_module')
     assert not hasattr(module, 'teardown_module')
