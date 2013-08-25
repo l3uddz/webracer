359b2b12332dfd01e428db18372a49dcbb8da2d5
Oleg Pudeyev <oleg@bsdpower.com>
2013-08-25 03:03:04 -0400

    Do not test xrange on python 3

--- tests/assertions_test.py
+++ tests/assertions_test.py
@@ -24,12 +24,13 @@ class AssertionsTest(webracer.WebTestCase):
         
         # range
         self.assert_status(range(200, 299))
-        
-    def test_assert_status_in_xrange(self):
-        self.get('http://127.0.0.1:8062/ok')
-        
-        # xrange
-        self.assert_status(xrange(200, 299))
+    
+    if not utils.py3:
+        def test_assert_status_in_xrange(self):
+            self.get('http://127.0.0.1:8062/ok')
+            
+            # xrange
+            self.assert_status(xrange(200, 299))
         
     def test_assert_status_not_equal_single(self):
         self.get('http://127.0.0.1:8062/redirect')
@@ -51,13 +52,14 @@ class AssertionsTest(webracer.WebTestCase):
         # not in range
         with self.assert_raises(AssertionError):
             self.assert_status(range(300, 399))
-        
-    def test_assert_status_not_in_xrange(self):
-        self.get('http://127.0.0.1:8062/ok')
-        
-        # not in xrange
-        with self.assert_raises(AssertionError):
-            self.assert_status(xrange(300, 399))
+    
+    if not utils.py3:
+        def test_assert_status_not_in_xrange(self):
+            self.get('http://127.0.0.1:8062/ok')
+            
+            # not in xrange
+            with self.assert_raises(AssertionError):
+                self.assert_status(xrange(300, 399))
         
     def test_assert_status_redirect(self):
         self.get('http://127.0.0.1:8062/redirect')
